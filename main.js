107036e122e588a0eea72c720c8648bcec0be1fa
Anita Amini <anita.amini@cohaesus.co.uk>
2016-02-14 13:50:02 +0000

    moved structured

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-var video = document.getElementById("video"), track;
-video.addEventListener("loadedmetadata", function() { 
-   track = document.createElement("track"); 
-   track.kind = "subtitles"; 
-   track.label = "English"; 
-   track.srclang = "en"; 
-   track.src = "sub.vtt";
-    track.attribute = "default";
-   track.addEventListener("load", function() { 
-      this.mode = "showing"; 
-      video.textTracks[0].mode = "showing"; // thanks Firefox 
-   }); 
-   this.appendChild(track); 
-});
+var overlay = document.getElementById('overlay');
+var video = document.getElementById('vid');
+video.addEventListener('progress', function() {
+    var show = video.currentTime >=1 && video.currentTime < 10;
+    overlay.style.visibility = show ? 'visible' : 'hidden';
+}, false);
+var circle = document.getElementById('circle');
+var modal = document.getElementById('modal');
+var close = document.getElementById('close');
+circle.addEventListener('click', function(){
+  modal.style.display = 'block';
+  circle.style.display = 'none';
+  video.pause();
+}, false);
+modal.addEventListener('click', function(){
+  modal.style.display = 'none';
+  video.play();
+}, false);
