223e73d18879e879c44a2c3e61df3a8377172e28
Neats29 <baadge29@yahoo.co.uk>
2016-02-15 09:50:29 +0000

    small change

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 var overlay = document.getElementById('overlay');
 var video = document.getElementById('vid');
-video.addEventListener('progress', function() {
-    var show = video.currentTime >=1 && video.currentTime < 10;
-    overlay.style.visibility = show ? 'visible' : 'hidden';
-}, false);
 var circle = document.getElementById('circle');
 var modal = document.getElementById('modal');
 var close = document.getElementById('close');
+
+video.addEventListener('progress', function() {
+  //currentTime is in seconds
+var show = video.currentTime >=1 && video.currentTime < 10;
+    overlay.style.visibility = show ? 'visible' : 'hidden';
+}, false);
+
 circle.addEventListener('click', function(){
   modal.style.display = 'block';
   circle.style.display = 'none';
   video.pause();
 }, false);
+
 modal.addEventListener('click', function(){
   modal.style.display = 'none';
   video.play();
