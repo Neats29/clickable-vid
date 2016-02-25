09ab6cf3ed91108794d850da735611b74aed1d5c
Neats29 <baadge29@yahoo.co.uk>
2016-02-25 14:56:57 +0000

    added js changes

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 var overlay = document.getElementById('js-overlay-container');
 var video = document.getElementById('js-video');
 var indicator = document.getElementById('js-overlay__indicator');
-var modal = document.getElementById('js-overlay--message');
+var overlayMessage = document.getElementById('js-overlay--message');
 var close = document.getElementById('js-overlay--close');
 
+//show indicator up to 6 secs into vid, after 6 it should dissapear
+//TODO once have assets, make func that takes in 2 times for window in which indicators appear and dissapear
 video.addEventListener('progress', function() {
-  //currentTime is in seconds
-var show = video.currentTime >=0 && video.currentTime < 10;
+var show = video.currentTime >= 0 && video.currentTime < 6;
     overlay.style.visibility = show ? 'visible' : 'hidden';
 }, false);
 
+
+//show overlay once indicator is clicked
 indicator.addEventListener('click', function(){
-  modal.style.display = 'block';
+  overlayMessage.style.display = 'block';
   indicator.style.display = 'none';
-  video.pause();
+//  video.pause();
 }, false);
 
-modal.addEventListener('click', function(){
-  modal.style.display = 'none';
-  video.src = "http://techslides.com/demos/sample-videos/small.mp4";
+//close overlay image and text
+function closeOverlayMessage() {
+    overlayMessage.style.display = 'none';
+    video.src = "http://techslides.com/demos/sample-videos/small.mp4";
+}
+
+//close overlay manually
+overlayMessage.addEventListener('click', function(){
+ closeOverlayMessage();
 }, false);
+
+//close overlay automatically after 6 secs
+video.addEventListener('progress', function() {
+  var show = video.currentTime > 6;
+  return show ? closeOverlayMessage() : false;
+}, false);
+
